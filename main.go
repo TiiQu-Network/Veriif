0523b10cb87a1fac03332b04bced8953a8ca1a83
Samyoul <samuel@samyoul.com>
2019-01-22 19:57:47 +0000

    Added RSA public key parsing and sig verifcation

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"bytes"
+	"crypto"
+	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"fmt"
 	"github.com/Samyoul/Veriif/models"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/pkg/errors"
@@ -17,12 +22,10 @@ func main() {
 	// Get the cert data from a file
 	content, _ := ioutil.ReadFile("example/cert_0.pdf")
 	cert, _ := findCert(content)
-	spew.Dump(cert)
 
 	// Parse the cert data into the expected struct
 	data := models.CertPacket{}
-	jn := json.Unmarshal(cert, &data)
-	spew.Dump(jn, data)
+	json.Unmarshal(cert, &data)
 
 	// Calculate the hash of the data
 	calcHash, _ := calculateDataHash(data)
@@ -30,11 +33,21 @@ func main() {
 	// Check the calculated hash matches the cert hash
 	certHash, _ := data.Hash.Decode()
 	if bytes.Equal(calcHash[:], certHash) {
-		spew.Dump("All good, hashes match")
+		println("All good, hashes match")
 	}
 
 	// TODO check public key is known
-	// TODO check the signature verifies
+
+	// Check the signature verifies
+	pk, _ := parsePublicKey([]byte(data.PublicKey))
+	sig, _ := data.Signature.Decode()
+	err := rsa.VerifyPKCS1v15(pk, crypto.SHA256, certHash, sig)
+	if err != nil {
+		spew.Dump("Signature verification fail!", err)
+	} else {
+		println("Signature verification SUCCESS.")
+	}
+
 	// TODO check the Merkle proof is verifies
 	// TODO check merkle root is on blockchain
 
@@ -63,4 +76,23 @@ func calculateDataHash(cert models.CertPacket) ([32]byte, error) {
 
 	hash := sha256.Sum256(jn)
 	return hash, nil
+}
+
+// parsePublicKey parses a PEM encoded private key.
+func parsePublicKey(pemBytes []byte) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, errors.New("no key found")
+	}
+
+	switch block.Type {
+	case "PUBLIC KEY":
+		pk, err := x509.ParsePKCS1PublicKey(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		return pk, nil
+	default:
+		return nil, fmt.Errorf("unsupported key type %q", block.Type)
+	}
 }
