263095111caf0891e0ba8d3ddfd9eba43c94512e
Samuel Hawksby-Robinson <samuel@samyoul.com>
2019-01-25 14:47:29 +0000

    Implemented application mode and mode selection

--- app.go
+++ app.go
@@ -1,9 +1,33 @@
 package main
 
-type App struct {}
+import "flag"
 
-// Initialisation
+// TODO add proper error handling / logging
+type App struct {
+	Mode uint
+}
+
+// Application Initialisation
 func (a App) Init() {
+	a.getParams()
+
+	switch a.Mode {
+	case 1:
+		a.initCLI()
+		break
+	case 2:
+		a.initBrowser()
+		break
+	}
+}
+
+func (a App) getParams() {
+	mode := flag.Uint("mode", 1, "System Mode :\n - 1 for CLI mode\n - 2 for browser UI mode\n -")
+	flag.Parse()
+	a.Mode = *mode
+}
+
+func (a App) initCLI() {
 	printTitle()
 	err := initContract()
 	if err != nil {
@@ -11,13 +35,34 @@ func (a App) Init() {
 	}
 }
 
+func (a App) initBrowser() {
+
+}
+
+// Application Running
+
 func (a App) Run() {
-	for {
-		a.Verify()
+	switch a.Mode {
+	case 1:
+		a.runCLI()
+		break
+	case 2:
+		a.runBrowser()
+		break
 	}
 }
 
-func (a App) Verify() {
+func (a App) runCLI() {
+	for {
+		a.verify()
+	}
+}
+
+func (a App) runBrowser() {
+
+}
+
+func (a App) verify() {
 	printPrompt()
 	filename := getInput()
 	printProcessing(filename)
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-// TODO add proper error handling / logging
 func main() {
 	app := App{}
 	app.Init()
